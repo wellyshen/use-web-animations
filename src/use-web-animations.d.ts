e36f137415bbd80768db905db30cdcfd9c6ee9c9
Welly Shen <hivoid19@gmail.com>
2020-06-06 15:12:46 +0800

    feat: add type definition

--- src/use-web-animations.d.ts
+++ src/use-web-animations.d.ts
@@ -1,3 +1,38 @@
-declare module "use-web-animations" {
-  // ...
+declare module "@welly/use-web-animations" {
+  import { RefObject } from "react";
+
+  export interface Callback {
+    (animation: Animation, event: AnimationPlaybackEvent): void;
+  }
+
+  type Keyframes = Keyframe[] | PropertyIndexedKeyframes;
+
+  type Timing = number | KeyframeAnimationOptions;
+
+  type PausedAtStart = boolean;
+
+  interface Options<T> {
+    ref?: RefObject<T>;
+    keyframes?: Keyframes;
+    timing?: Timing;
+    pausedAtStart?: PausedAtStart;
+    onFinish?: Callback;
+    onCancel?: Callback;
+  }
+
+  interface Return<T> {
+    readonly ref: RefObject<T>;
+    readonly animation?: Animation;
+    readonly setAnimation?: (
+      keyframes: Keyframes,
+      timing?: Timing,
+      pausedAtStart?: PausedAtStart
+    ) => void;
+  }
+
+  const useWebAnimations: <T extends HTMLElement>(
+    options?: Options<T>
+  ) => Return<T>;
+
+  export default useWebAnimations;
 }
