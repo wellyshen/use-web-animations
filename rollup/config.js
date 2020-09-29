70a185142ead709158cbeddf7bc4f506b17fb5bf
Welly Shen <hivoid19@gmail.com>
2020-09-29 22:14:53 +0800

    chore: skip minify for es module

--- rollup/config.js
+++ rollup/config.js
@@ -31,6 +31,7 @@ const cjs = {
   format: "cjs",
   sourcemap: isDev,
   exports: "named",
+  plugins: !isDev && [terser()],
 };
 
 const esm = {
@@ -61,8 +62,6 @@ const plugins = [
   isDev && serve("demo/.dev"),
   isDev && livereload(),
   !isDev && sizeSnapshot(),
-  // Disable "module" to avoid the missing semicolon bug of .esm
-  !isDev && terser({ module: false }),
   isDemo &&
     copy({
       targets: [{ src: "demo/.dev", dest: ".", rename: "public" }],
