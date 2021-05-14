05b7a308e719d66b54b2b671ca94fe9ce5bb4033
Welly <hivoid19@gmail.com>
2021-05-15 01:01:02 +0800

    Update useLatest.ts

--- src/useLatest.ts
+++ src/useLatest.ts
@@ -1,11 +1,7 @@
-import { RefObject, useRef, useEffect } from "react";
+import { RefObject, useRef } from "react";
 
 export default <T>(val: T): RefObject<T> => {
   const ref = useRef(val);
-
-  useEffect(() => {
-    ref.current = val;
-  });
-
+  ref.current = val;
   return ref;
 };
